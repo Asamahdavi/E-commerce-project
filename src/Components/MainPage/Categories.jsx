9a1dc48c7a8f4689d4355c11e68f4f090754e92d
Asamahdavi <asamahdavii@gmail.com>
2022-07-02 19:02:27 +0430

    category icons added

--- src/Components/MainPage/Categories.jsx
+++ src/Components/MainPage/Categories.jsx
@@ -1,67 +1,67 @@
-import React from "react"
-
+import React from "react";
+import { k } from "../Assets/category/cat2.png";
 const Categories = () => {
   const data = [
     {
-      cateImg: "./images/category/cat1.png",
+      cateImg: "",
       cateName: "Fashion",
     },
     {
-      cateImg: "./images/category/cat2.png",
+      cateImg: "../Assets/category/cat2.png",
       cateName: "Electronic",
     },
     {
-      cateImg: "./images/category/cat3.png",
+      cateImg: "../Assets/category/cat3.png",
       cateName: "Cars",
     },
     {
-      cateImg: "./images/category/cat4.png",
+      cateImg: "../Assets/category/cat4.png",
       cateName: "Home & Garden",
     },
     {
-      cateImg: "./images/category/cat5.png",
+      cateImg: "../Assets/category/cat5.png",
       cateName: "Gifts",
     },
     {
-      cateImg: "./images/category/cat6.png",
+      cateImg: "../Assets/category/cat6.png",
       cateName: "Music",
     },
     {
-      cateImg: "./images/category/cat7.png",
+      cateImg: "../Assets/category/cat7.png",
       cateName: "Health & Beauty",
     },
     {
-      cateImg: "./images/category/cat8.png",
+      cateImg: "../Assets/category/cat8.png",
       cateName: "Pets",
     },
     {
-      cateImg: "./images/category/cat9.png",
+      cateImg: "../Assets/category/cat9.png",
       cateName: "Baby Toys",
     },
     {
-      cateImg: "./images/category/cat10.png",
+      cateImg: "../Assets/category/cat10.png",
       cateName: "Groceries",
     },
     {
-      cateImg: "./images/category/cat11.png",
+      cateImg: "../Assets/category/cat11.png",
       cateName: "Books",
     },
-  ]
+  ];
 
   return (
     <>
-      <div className='category'>
+      <div className="category">
         {data.map((value, index) => {
           return (
-            <div className='box f_flex' key={index}>
-              <img src={value.cateImg} alt='' />
+            <div className="box f_flex" key={index}>
+              <img src={value.cateImg} alt="" />
               <span>{value.cateName}</span>
             </div>
-          )
+          );
         })}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
